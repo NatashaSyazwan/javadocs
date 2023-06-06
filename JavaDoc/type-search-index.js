bff420aee4d8b300191d88d5897061c550f888a1
NatashaSyazwan <111846354+NatashaSyazwan@users.noreply.github.com>
2023-06-06 21:54:07 +0800

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"App"},{"p":"my.uum","l":"AppTest"},{"p":"my.uum","l":"Lecturer"},{"p":"my.uum","l":"LecturerData"},{"p":"my.uum","l":"LecturerDistance"},{"p":"my.uum","l":"Main"},{"p":"my.uum","l":"SOCLecturerFinderBot"}];updateSearchResults();
